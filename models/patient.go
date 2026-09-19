c724b57e0a13a8148e6dc183c34523ddf5b434dc
p.supsri <p.supsri@gmail.com>
2026-09-19 19:39:23 +0700

    fix gender type

--- models/patient.go
+++ models/patient.go
@@ -2,24 +2,31 @@ package models
 
 import "time"
 
+type GenderType string
+
+const (
+	GenderMale   GenderType = "M"
+	GenderFemale GenderType = "F"
+)
+
 type Patient struct {
-	ID           int        `gorm:"primaryKey;autoIncrement" json:"id"`
-	PatientHN    string     `gorm:"not null;unique" json:"patient_hn"`
-	NationalID   *string    `gorm:"unique" json:"national_id"`
-	PassportID   *string    `gorm:"unique" json:"passport_id"`
-	FirstNameTH  string     `gorm:"not null" json:"first_name_th"`
-	MiddleNameTH *string    `json:"middle_name_th"`
-	LastNameTH   string     `gorm:"not null" json:"last_name_th"`
-	FirstNameEN  string     `gorm:"not null" json:"first_name_en"`
-	MiddleNameEN *string    `json:"middle_name_en"`
-	LastNameEN   string     `gorm:"not null" json:"last_name_en"`
-	DateOfBirth  *time.Time `json:"date_of_birth"`
-	PhoneNumber  *string    `json:"phone_number"`
-	Email        *string    `json:"email"`
-	Gender       *string    `json:"gender"`
-	HospitalID   int        `gorm:"not null" json:"hospital_id"`
-	CreatedAt    time.Time  `gorm:"not null" json:"created_at"`
-	UpdatedAt    time.Time  `gorm:"not null" json:"updated_at"`
+	ID           int         `gorm:"primaryKey;autoIncrement" json:"id"`
+	PatientHN    string      `gorm:"not null;unique" json:"patient_hn"`
+	NationalID   *string     `gorm:"unique" json:"national_id"`
+	PassportID   *string     `gorm:"unique" json:"passport_id"`
+	FirstNameTH  string      `gorm:"not null" json:"first_name_th"`
+	MiddleNameTH *string     `json:"middle_name_th"`
+	LastNameTH   string      `gorm:"not null" json:"last_name_th"`
+	FirstNameEN  string      `gorm:"not null" json:"first_name_en"`
+	MiddleNameEN *string     `json:"middle_name_en"`
+	LastNameEN   string      `gorm:"not null" json:"last_name_en"`
+	DateOfBirth  *time.Time  `json:"date_of_birth"`
+	PhoneNumber  *string     `json:"phone_number"`
+	Email        *string     `json:"email"`
+	Gender       *GenderType `gorm:"type:varchar(1);check:gender IN ('M', 'F')" json:"gender"`
+	HospitalID   int         `gorm:"not null" json:"hospital_id"`
+	CreatedAt    time.Time   `gorm:"not null" json:"created_at"`
+	UpdatedAt    time.Time   `gorm:"not null" json:"updated_at"`
 }
 
 type GetPatientQuery struct {
@@ -34,17 +41,17 @@ type GetPatientQuery struct {
 }
 
 type CreatePatientRequest struct {
-	PatientHN    string     `json:"patient_hn" binding:"required"`
-	NationalID   *string    `json:"national_id"`
-	PassportID   *string    `json:"passport_id"`
-	FirstNameTH  string     `json:"first_name_th" binding:"required"`
-	MiddleNameTH *string    `json:"middle_name_th"`
-	LastNameTH   string     `json:"last_name_th" binding:"required"`
-	FirstNameEN  string     `json:"first_name_en" binding:"required"`
-	MiddleNameEN *string    `json:"middle_name_en"`
-	LastNameEN   string     `json:"last_name_en" binding:"required"`
-	DateOfBirth  *time.Time `json:"date_of_birth"`
-	PhoneNumber  *string    `json:"phone_number"`
-	Email        *string    `json:"email"`
-	Gender       *string    `json:"gender"`
+	PatientHN    string      `json:"patient_hn" binding:"required"`
+	NationalID   *string     `json:"national_id"`
+	PassportID   *string     `json:"passport_id"`
+	FirstNameTH  string      `json:"first_name_th" binding:"required"`
+	MiddleNameTH *string     `json:"middle_name_th"`
+	LastNameTH   string      `json:"last_name_th" binding:"required"`
+	FirstNameEN  string      `json:"first_name_en" binding:"required"`
+	MiddleNameEN *string     `json:"middle_name_en"`
+	LastNameEN   string      `json:"last_name_en" binding:"required"`
+	DateOfBirth  *time.Time  `json:"date_of_birth"`
+	PhoneNumber  *string     `json:"phone_number"`
+	Email        *string     `json:"email"`
+	Gender       *GenderType `json:"gender" binding:"omitempty,oneof=M F"`
 }
